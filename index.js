5044273ea86345ffe97e3eb367868db96c3059cb
Martin Nuc <martin@nuc.cz>
2018-07-20 00:32:44 +0200

    reformat

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const rpio = require('rpio');
+const rpio = require("rpio");
 const PIN1 = 37;
 let Service, Characteristic;
- 
-module.exports = function (homebridge) {
+
+module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   homebridge.registerAccessory("switch-plugin", "MyAwesomeSwitch", MySwitch);
@@ -10,86 +10,90 @@ module.exports = function (homebridge) {
 
 const BUZZER_DELAY = 3000;
 
-const BUZZER = 'BUZZER';
-const OFF = 'OFF';
+const BUZZER = "BUZZER";
+const OFF = "OFF";
 
 class MySwitch {
-	constructor() {
-		rpio.open(PIN1, rpio.OUTPUT);
-		setTimeout(() => this.clear(), 0);
-	}
+  constructor() {
+    rpio.open(PIN1, rpio.OUTPUT);
+    setTimeout(() => this.clear(), 0);
+  }
 
-	getServices() {
-		let informationService = new Service.AccessoryInformation();
-		informationService
-			.setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
-			.setCharacteristic(Characteristic.Model, "My switch model")
-			.setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+  getServices() {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
+      .setCharacteristic(Characteristic.Model, "My switch model")
+      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
 
-		const switchService = new Service.Door("Door");
-		switchService
-			.getCharacteristic(Characteristic.CurrentPosition)
-				.on('get', (next) => next(null, this.getProgress()))
-		switchService
-				.getCharacteristic(Characteristic.TargetPosition)
-				.on('get', (next) => next(null, this.getTarget()))
-				.on('set', (on, next) => {
-					this.openDoor();
-					next();
-				});
+    const switchService = new Service.Door("Door");
+    switchService
+      .getCharacteristic(Characteristic.CurrentPosition)
+      .on("get", next => next(null, this.getProgress()));
+    switchService
+      .getCharacteristic(Characteristic.TargetPosition)
+      .on("get", next => next(null, this.getTarget()))
+      .on("set", (on, next) => {
+        this.openDoor();
+        next();
+      });
 
-		this.informationService = informationService;
-		this.switchService = switchService;
-		return [informationService, switchService];
-	}
+    this.informationService = informationService;
+    this.switchService = switchService;
+    return [informationService, switchService];
+  }
 
-	getProgress() {
-		switch (this.state) {
-			case BUZZER:
-				return 100;
-			case OFF:
-				return 0;
-		}
-	}
+  getProgress() {
+    switch (this.state) {
+      case BUZZER:
+        return 100;
+      case OFF:
+        return 0;
+    }
+  }
 
-	getTarget() {
-		switch (this.state) {
-			case BUZZER:
-				return 0;
-			case OFF:
-				return 0;
-		}
-	}
+  getTarget() {
+    switch (this.state) {
+      case BUZZER:
+        return 0;
+      case OFF:
+        return 0;
+    }
+  }
 
-	openDoor() {
-		if (this.state !== OFF) {
-			this.clear();
-			return;
-		}
-		this.startOpening();
-	}
+  openDoor() {
+    if (this.state !== OFF) {
+      this.clear();
+      return;
+    }
+    this.startOpening();
+  }
 
-	updateHomekit() {
-		this.switchService.getCharacteristic(Characteristic.CurrentPosition).updateValue(this.getProgress());
-		this.switchService.getCharacteristic(Characteristic.TargetPosition).updateValue(this.getTarget());
-	}
+  updateHomekit() {
+    this.switchService
+      .getCharacteristic(Characteristic.CurrentPosition)
+      .updateValue(this.getProgress());
+    this.switchService
+      .getCharacteristic(Characteristic.TargetPosition)
+      .updateValue(this.getTarget());
+  }
 
-	clear() {
-		this.state = OFF;
-		rpio.write(PIN1, rpio.HIGH);
-		clearTimeout(this.buzzerTimeout);
-		this.updateHomekit();
-	}
+  clear() {
+    this.state = OFF;
+    rpio.write(PIN1, rpio.HIGH);
+    clearTimeout(this.buzzerTimeout);
+    this.updateHomekit();
+  }
 
-	startOpening() {
-		this.state = BUZZER;
-		rpio.write(PIN1, rpio.LOW);
+  startOpening() {
+    this.state = BUZZER;
+    rpio.write(PIN1, rpio.LOW);
 
-		this.updateHomekit();
+    this.updateHomekit();
 
-		this.buzzerTimeout = setTimeout(() => {
-				this.clear();
-				this.updateHomekit();
-		}, BUZZER_DELAY);
-	}
+    this.buzzerTimeout = setTimeout(() => {
+      this.clear();
+      this.updateHomekit();
+    }, BUZZER_DELAY);
+  }
 }
